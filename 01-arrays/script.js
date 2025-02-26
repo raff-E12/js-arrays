5081f5173fcfc36bb1fa479f89490ff38dc2036a
Raff_12 <raffelecoppola381@gmail.com>
2025-02-26 16:34:23 +0100

    feat: Completamento degli esercizi sulla prima parte

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -18,21 +18,32 @@ teachers[3] = 'Patrick';
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers[7];
-teachers.unshift();
+const lastTeacher = teachers.pop();
 console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.unshift('Vanessa');
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.push('Sarah');
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers[5];
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers;
+
+if (isTeachersEmpty.length !== 0) {
+  console.log("La lista degli insegnanti è piena.");
+} else {
+  console.log("La lista degli insegnanti non è piena.");
+}
